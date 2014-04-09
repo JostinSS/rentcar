6cd28e2f0526c519c35a6e6b3753a4e0960ae318
Lewdin <Lewdin@Lewdin-PC>
2014-04-09 19:28:19 -0400

    Commit hecho por lewdin

---
+++ src/com/proyectofinal/Sistema.java
@@ -0,0 +1,28 @@
+package com.proyectofinal;
+
+import com.proyectofinal.ui.VentanaAdministrador;
+
+public class Sistema {
+	public static void main(String[] args) {
+		/*MatenimientoUsuarios mantenimiento = new MatenimientoUsuarios();
+		mantenimiento.setLocationRelativeTo(null);
+		mantenimiento.setVisible(true);*/
+		/*Login login = new Login();
+		login.setLocationRelativeTo(null);
+		login.setVisible(true);*/
+		VentanaAdministrador admin = new VentanaAdministrador();
+		admin.setLocationRelativeTo(null);
+		admin.setVisible(true);
+		
+	/*	MantenimientoClientes m = new MantenimientoClientes();
+		m.setLocationRelativeTo(null);
+		m.setVisible(true);
+		*/
+	/*	
+	 * ----hoaaaa
+		MatenimientoAccesorio accesorios = new MatenimientoAccesorio();
+		accesorios.setLocationRelativeTo(null);
+		accesorios.setVisible(true);*/
+		
+	}
+}
